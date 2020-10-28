ea437573393398df24d42568fbc0c8eca7782684
holyjc827 <holyjc827@gmail.com>
2020-10-29 00:38:58 +0100

    fixed and added the following features: 1) Automatic button click for cookie popup 2) For dynamic contents, scrolling down to the bottom of the page automatically, but the page gets scrolled down slowly, so that the contents get loaded 3) remove duplicates in the returned array

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+import math
 import time
 
 PATH = "/Users/jcpark/Desktop/Coding/WebScraping/chromedriver"
@@ -11,22 +8,20 @@ driver = webdriver.Chrome(PATH)
 driver.get("https://www.ah.nl/bonus")
 item_list = []
 
-# button = driver.find_element_by_id("all")
-# button.click()
+button = driver.find_element_by_id("accept-cookies")
+button.click()
+
+scrollHeight = driver.execute_script("return document.body.scrollHeight")
+for i in range(1, math.ceil(scrollHeight / 1000)):
+    driver.execute_script("window.scrollTo(" + str((i - 1) * 1000) + "," + str(i * 1000) + ")")
+    time.sleep(0.5)
 
 try:
-    # main = WebDriverWait(driver, 30).until(
-    #     EC.presence_of_element_located((By.CLASS_NAME, "page"))
-    # )
-    time.sleep(30)
     items = driver.find_elements_by_class_name("link_root__2DsAD")
     for item in items:
         name = item.get_attribute("title")
-        item_name = {
-            'name' : name
-        }
-
-        item_list.append(item_name)
-        print(item_list)
+        item_list.append(name)
+    item_list = list(dict.fromkeys(item_list))
+    print(item_list)
 except:
     driver.quit()
